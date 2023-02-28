d3e0d625dcd909999acc51ba9b30a2b2c9052a07
Devsignerz  <Devsignerz@gmail.com>
2023-02-27 20:24:20 -0600

    click click bitch

--- Main.cs
+++ Main.cs
@@ -2,14 +2,16 @@ using Godot;
 using System;
 
 public partial class Main : Node{
-	[Export] int i;
+	int i;
+	[Export] Label lbl;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
+		i=0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta){
-		i++;
+		lbl.Text = i.ToString();
 	}
 	public void Click(){
 		i++;
